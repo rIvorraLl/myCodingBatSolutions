9f36432038f79fb73f578e37a00991c07d6840a5
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-17 19:51:43 +0200

    add: string_1 package, HelloName class

---
+++ src/string_1/HelloName.java
@@ -0,0 +1,10 @@
+package string_1;
+
+public class HelloName {
+    /*
+     * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     */
+    public String helloName(String name) {
+	return "Hello " + name + "!";
+    }
+}
