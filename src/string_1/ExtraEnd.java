ea0c4bee2379ddda73385667280a97263530dda7
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-17 20:03:53 +0200

    add: ExtraEnd class

---
+++ src/string_1/ExtraEnd.java
@@ -0,0 +1,12 @@
+package string_1;
+
+public class ExtraEnd {
+    /*
+     * Given a string, return a new string made of 3 copies of the last 2 chars of
+     * the original string. The string length will be at least 2.
+     */
+    public String extraEnd(String str) {
+	String end = str.substring(str.length() - 2, str.length());
+	return end + end + end;
+    }
+}
