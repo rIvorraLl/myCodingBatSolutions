9ee86975ac8ad61d0ff0d61359667a8aa0007f07
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-17 19:54:27 +0200

    add: MakeAbba class

---
+++ src/string_1/MakeAbba.java
@@ -0,0 +1,11 @@
+package string_1;
+
+public class MakeAbba {
+    /*
+     * Given two strings, a and b, return the result of putting them together in the
+     * order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+     */
+    public String makeAbba(String a, String b) {
+	return a + b + b + a;
+    }
+}
