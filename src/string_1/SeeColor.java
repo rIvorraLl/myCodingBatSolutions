15cf6271c80f5b3c76d5d05edf84344e76dc473d
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-18 19:24:31 +0200

    add: SeeColor class

---
+++ src/string_1/SeeColor.java
@@ -0,0 +1,16 @@
+package string_1;
+
+public class SeeColor {
+    /*
+     * Given a string, if the string begins with "red" or "blue" return that color
+     * string, otherwise return the empty string.
+     */
+    public String seeColor(String str) {
+	if (str.startsWith("red")) {
+	    return "red";
+	} else if (str.startsWith("blue")) {
+	    return "blue";
+	}
+	return "";
+    }
+}
