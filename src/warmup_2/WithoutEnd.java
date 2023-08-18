179e538097f204e3086bf32337cd0c09b8c951b8
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-18 09:21:47 +0200

    add: WithoutEnd class

---
+++ src/warmup_2/WithoutEnd.java
@@ -0,0 +1,11 @@
+package warmup_2;
+
+public class WithoutEnd {
+    /*
+     * Given a string, return a version without the first and last char, so "Hello"
+     * yields "ell". The string length will be at least 2.
+     */
+    public String withoutEnd(String str) {
+	return str.substring(1, str.length() - 1);
+    }
+}
