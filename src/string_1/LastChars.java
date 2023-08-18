f8e2bc95812c73aa5440262d59dd890d19b8b01b
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-18 18:51:39 +0200

    add: LastChars class

---
+++ src/string_1/LastChars.java
@@ -0,0 +1,14 @@
+package string_1;
+
+public class LastChars {
+    /*
+     * Given 2 strings, a and b, return a new string made of the first char of a and
+     * the last char of b, so "yo" and "java" yields "ya". If either string is
+     * length 0, use '@' for its missing char.
+     */
+    public String lastChars(String a, String b) {
+	String firstA = a.length() > 0 ? a.substring(0, 1) : "@";
+	String lastB = b.length() > 0 ? b.substring(b.length() - 1) : "@";
+	return firstA + lastB;
+    }
+}
