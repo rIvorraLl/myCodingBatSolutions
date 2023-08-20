e295510ed34d60687cb29241b490eddc53aa519d
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-20 23:22:43 +0200

    add: SameFirstLast class

---
+++ src/array_1/SameFirstLast.java
@@ -0,0 +1,17 @@
+package array_1;
+
+public class SameFirstLast {
+    /*
+     * Given an array of ints, return true if the array is length 1 or more, and the
+     * first element and the last element are equal.
+     */
+    public boolean sameFirstLast(int[] nums) {
+	if (nums.length < 1) {
+	    return false;
+	}
+	if (nums.length == 1 || (nums[0] == nums[nums.length - 1])) {
+	    return true;
+	}
+	return false;
+    }
+}
