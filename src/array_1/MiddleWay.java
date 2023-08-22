bdbf55e0a14b50a556f35dcc90d03ffd91896ace
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-22 09:31:11 +0200

    add: MiddleWay class

---
+++ src/array_1/MiddleWay.java
@@ -0,0 +1,12 @@
+package array_1;
+
+public class MiddleWay {
+    /*
+     * Given 2 int arrays, a and b, each length 3, return a new array length 2
+     * containing their middle elements.
+     */
+    public int[] middleWay(int[] a, int[] b) {
+	int[] result = { a[1], b[1] };
+	return result;
+    }
+}
