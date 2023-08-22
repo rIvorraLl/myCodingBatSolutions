96dd86cab007489c30bf9317cc1d820ca7381bd8
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-22 10:32:58 +0200

    add: PlusTwo class

---
+++ src/array_1/PlusTwo.java
@@ -0,0 +1,12 @@
+package array_1;
+
+public class PlusTwo {
+    /*
+     * Given 2 int arrays, each length 2, return a new array length 4 containing all
+     * their elements.
+     */
+    public int[] plusTwo(int[] a, int[] b) {
+	int[] result = { a[0], a[1], b[0], b[1] };
+	return result;
+    }
+}
