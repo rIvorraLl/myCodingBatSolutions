7f9458bbe06995cb9139c064c9a3f1cf531c00ae
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-22 08:53:45 +0200

    add: CommonEnd class

---
+++ src/array_1/CommonEnd.java
@@ -0,0 +1,13 @@
+package array_1;
+
+public class CommonEnd {
+    /*
+     * Given 2 arrays of ints, a and b, return true if they have the same first
+     * element or they have the same last element. Both arrays will be length 1 or
+     * more.
+     */
+    public boolean commonEnd(int[] a, int[] b) {
+	boolean result = (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) ? true : false;
+	return result;
+    }
+}
