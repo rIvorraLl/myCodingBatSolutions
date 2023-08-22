efc7856cd48db4828eaa03fe1d9a862f8709f877
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-22 08:49:08 +0200

    add: MakePi class

---
+++ src/array_1/MakePi.java
@@ -0,0 +1,11 @@
+package array_1;
+
+public class MakePi {
+    /*
+     * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+     */
+    public int[] makePi() {
+	int[] result = { 3, 1, 4 };
+	return result;
+    }
+}
