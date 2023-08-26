9373903c138e6eb11277aa5de06c90320e978bbb
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-26 09:40:37 +0200

    add: RedTicket class

--- src/logic_1/LessBy10.java
+++ src/logic_1/LessBy10.java
@@ -6,7 +6,7 @@ public class LessBy10 {
      * one of the others.
      */
     public boolean lessBy10(int a, int b, int c) {
-	if (a - b >= 10 || a - c >= 10 || b - c >= 10) {
+	if (a - b >= 10 || a - c >= 10 || b - c >= 10 || b - a >= 10 || c - a >= 10 || c - b >= 10) {
 	    return true;
 	}
 	return false;
