a5f834818c7e90f465e2eaa358bdfd0560041f22
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-08-26 09:17:08 +0200

    add: LessBy10 class

---
+++ src/logic_1/LessBy10.java
@@ -0,0 +1,14 @@
+package logic_1;
+
+public class LessBy10 {
+    /*
+     * Given three ints, a b c, return true if one of them is 10 or more less than
+     * one of the others.
+     */
+    public boolean lessBy10(int a, int b, int c) {
+	if (a - b >= 10 || a - c >= 10 || b - c >= 10) {
+	    return true;
+	}
+	return false;
+    }
+}
