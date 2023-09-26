0aa09c49f7da07b27af6123746fa9f147efc5cf2
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-09-26 19:02:11 +0200

    add: MaxSpan class

---
+++ src/array_3/MaxSpan.java
@@ -0,0 +1,33 @@
+package array_3;
+
+public class MaxSpan {
+    /*
+     * Consider the leftmost and righmost appearances of some value in an array.
+     * We'll say that the "span" is the number of elements between the two
+     * inclusive. A single value has a span of 1. Returns the largest span found in
+     * the given array. (Efficiency is not a priority.)
+     */
+    public int maxSpan(int[] nums) {
+	int maxDistance = 0;
+	boolean equalFound = false;
+	for (int i = 0; i < nums.length; i++) {
+	    for (int j = i + 1; j < nums.length; j++) {
+		if (nums[j] == nums[i]) {
+		    equalFound = true;
+		    int distance = j - i + 1;
+		    if (distance > maxDistance) {
+			maxDistance = distance;
+		    }
+		}
+	    }
+	}
+	if (!equalFound) {
+	    int result = nums.length > 0 ? 1 : 0;
+	    return result;
+	}
+	return maxDistance;
+    }
+}
+
+// maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+//	    0  1  2  3  4  5  6
