8c2f2d30cc8a27e539ea7ff34bce8d66d4efadcc
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-01 09:47:08 +0200

    add: EqualIsNot class

---
+++ src/array_3/EqualIsNot.java
@@ -0,0 +1,24 @@
+package array_3;
+
+public class EqualIsNot {
+    /*
+     * Given a string, return true if the number of appearances of "is" anywhere in
+     * the string is equal to the number of appearances of "not" anywhere in the
+     * string (case sensitive).
+     */
+    public boolean equalIsNot(String str) {
+	int counterN = 0;
+	int counterI = 0;
+	for (int i = 0; i < str.length() - 1; i++) {
+	    if(str.substring(i, i + 2).equals("is")) {
+		counterI++;
+	    }
+	}
+	for (int i = 0; i < str.length() - 2; i++) {
+	    if(str.substring(i, i + 3).equals("not")) {
+		counterN++;
+	    }
+	}
+	return counterI == counterN;
+    }
+}
