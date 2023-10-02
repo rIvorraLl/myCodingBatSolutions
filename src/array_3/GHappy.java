a0aff3f5a60a096b740b6a67fdf1d52dd9583aaa
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-02 18:00:47 +0200

    add: GHappy class

---
+++ src/array_3/GHappy.java
@@ -0,0 +1,26 @@
+package array_3;
+
+public class GHappy {
+    /*
+     * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g'
+     * immediately to its left or right. Return true if all the g's in the given
+     * string are happy.
+     */
+    public boolean gHappy(String str) {
+	if (str.length() == 0) {
+	    return true;
+	}
+	if (str.length() < 2) {
+	    return false;
+	}
+	if (str.length() > 1 && str.charAt(str.length() - 1) == 'g' && str.charAt(str.length() - 2) != 'g') {
+	    return false;
+	}
+	for (int i = 1; i < str.length() - 1; i++) {
+	    if (str.charAt(i) == 'g' && (str.charAt(i - 1) != 'g' && str.charAt(i + 1) != 'g')) {
+		return false;
+	    }
+	}
+	return true;
+    }
+}
