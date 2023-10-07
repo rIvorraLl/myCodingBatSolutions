501083b10f20d940c38b2f3d987a9e5e0a68863d
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-07 09:30:55 +0200

    add: */map_1/topping3

--- src/map_1/Topping2.java
+++ src/map_1/Topping2.java
@@ -10,7 +10,7 @@ public class Topping2 {
      * "nuts".
      */
     public Map<String, String> topping2(Map<String, String> map) {
-	if(map.get("ice cream") != null) {
+	if (map.get("ice cream") != null) {
 	    map.put("yogurt", map.get("ice cream"));
 	}
 	if (map.get("spinach") != null) {
