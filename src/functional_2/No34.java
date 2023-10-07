c5da2e8b1e3416bcabe8aec643e6beb6200536dc
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-08 01:16:48 +0200

    add: ./functional_2/No34

---
+++ src/functional_2/No34.java
@@ -0,0 +1,14 @@
+package functional_2;
+
+import java.util.List;
+
+public class No34 {
+    /*
+     * Given a list of strings, return a list of the strings, omitting any string
+     * length 3 or 4.
+     */
+    public List<String> no34(List<String> strings) {
+	strings.removeIf(s -> s.length() == 3 || s.length() == 4);
+	return strings;
+    }
+}
