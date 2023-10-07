b163f42a4ee0a2a11587e5fe0b0e20f8714d6458
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-07 09:18:57 +0200

    add: */map_1/Topping1

---
+++ src/map_1/Topping1.java
@@ -0,0 +1,18 @@
+package map_1;
+
+import java.util.Map;
+
+public class Topping1 {
+    /*
+     * Given a map of food keys and topping values, modify and return the map as
+     * follows: if the key "ice cream" is present, set its value to "cherry". In all
+     * cases, set the key "bread" to have the value "butter".
+     */
+    public Map<String, String> topping1(Map<String, String> map) {
+	if (map.containsKey("ice cream")) {
+	    map.put("ice cream", "cherry");
+	}
+	map.put("bread", "butter");
+	return map;
+    }
+}
