0332b8cc5dc7d1cc2200b880b5f5c05142830158
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-08 01:12:34 +0200

    add: ./functional_2/NoZ

---
+++ src/functional_2/NoZ.java
@@ -0,0 +1,14 @@
+package functional_2;
+
+import java.util.List;
+
+public class NoZ {
+    /*
+     * Given a list of strings, return a list of the strings, omitting any string
+     * that contains a "z". (Note: the str.contains(x) method returns a boolean)
+     */
+    public List<String> noZ(List<String> strings) {
+	strings.removeIf(s -> s.contains("z"));
+	return strings;
+    }
+}
