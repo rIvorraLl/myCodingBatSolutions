37512de86ef401f353fec9bf7b8b357815e96fec
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-08 01:19:50 +0200

    add: ./functional_2/NoYY

---
+++ src/functional_2/NoYY.java
@@ -0,0 +1,16 @@
+package functional_2;
+
+import java.util.List;
+
+public class NoYY {
+    /*
+     * Given a list of strings, return a list where each string has "y" added at its
+     * end, omitting any resulting strings that contain "yy" as a substring
+     * anywhere.
+     */
+    public List<String> noYY(List<String> strings) {
+	strings.replaceAll(s -> s + "y");
+	strings.removeIf(s -> s.contains("yy"));
+	return strings;
+    }
+}
