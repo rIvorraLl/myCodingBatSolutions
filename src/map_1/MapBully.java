836430c415306a112a780c51790a9e4021f65cdc
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-07 09:03:47 +0200

    add: */map_1/MapBully

---
+++ src/map_1/MapBully.java
@@ -0,0 +1,23 @@
+package map_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapBully {
+    /*
+     * Modify and return the given map as follows: if the key "a" has a value, set
+     * the key "b" to have that value, and set the key "a" to have the value "".
+     * Basically "b" is a bully, taking the value and replacing it with the empty
+     * string.
+     */
+    public Map<String, String> mapBully(Map<String, String> map) {
+	if (map.get("a") == null) {
+	    return map;
+	}
+	if (map.get("a") != "") {
+	    map.put("b", map.get("a"));
+	    map.put("a", "");
+	}
+	return map;
+    }
+}
