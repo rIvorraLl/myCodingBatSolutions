eef91e4e5cb6e37204671bc8061ce313d3a17158
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-08 17:36:20 +0200

    add: ./recursion_1/SumDigits

---
+++ src/recursion_1/SumDigits.java
@@ -0,0 +1,15 @@
+package recursion_1;
+
+public class SumDigits {
+    /*
+     * Given a non-negative int n, return the sum of its digits recursively (no
+     * loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+     * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+     */
+    public int sumDigits(int n) {
+	if (n == 0) {
+	    return 0;
+	}
+	return sumDigits(n / 10) + n % 10;
+    }
+}
