7a902414b7bc290907254089e58863cb5f8281ff
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-08 09:56:19 +0200

    add: ./recursion_1/BunnyEars

---
+++ src/recursion_1/BunnyEars.java
@@ -0,0 +1,15 @@
+package recursion_1;
+
+public class BunnyEars {
+    /*
+     * We have a number of bunnies and each bunny has two big floppy ears. We want
+     * to compute the total number of ears across all the bunnies recursively
+     * (without loops or multiplication).
+     */
+    public int bunnyEars(int bunnies) {
+	if (bunnies == 0) {
+	    return 2;
+	}
+	return bunnyEars(bunnies - 1) + 2;
+    }
+}
