387d9501f0e0141a8d7637e52fa4701b2b519cb9
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-09 19:25:03 +0200

    add: ./recursion_1/PowerN

---
+++ src/recursion_1/PowerN.java
@@ -0,0 +1,14 @@
+package recursion_1;
+
+public class PowerN {
+    /*
+     * Given base and n that are both 1 or more, compute recursively (no loops) the
+     * value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+     */
+    public int powerN(int base, int n) {
+	if (n == 0) {
+	    return 1;
+	}
+	return powerN(base * base, n - 1);
+    }
+}
