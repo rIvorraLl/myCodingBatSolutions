9337e86b160cd9b51f3112f1ffc7716c3ee79ba1
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-10 00:47:40 +0200

    add: ./recursion_1/ChangeXY

--- src/recursion_1/ChangeXY.java
+++ src/recursion_1/ChangeXY.java
@@ -1,5 +1,19 @@
 package recursion_1;
 
 public class ChangeXY {
+    /*
+     * Given a string, compute recursively (no loops) a new string where all the
+     * lowercase 'x' chars have been changed to 'y' chars.
+     */
+    public String changeXY(String str) {
+	if (str.length() < 1) {
+	    return str;
+	}
+	String s = str.substring(0, 1);
+	if (s.equals("x")) {
+	    return "y" + changeXY(str.substring(1));
+	}
 
+	return s + changeXY(str.substring(1));
+    }
 }
