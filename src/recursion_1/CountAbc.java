5b6e2d687c97ccfd2594a2871e8ad043597ba8e4
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-11 00:17:58 +0200

    add: ./recursion_1/CountAbc

---
+++ src/recursion_1/CountAbc.java
@@ -0,0 +1,17 @@
+package recursion_1;
+
+public class CountAbc {
+    /*
+     * Count recursively the total number of "abc" and "aba" substrings that appear
+     * in the given string.
+     */
+    public int countAbc(String str) {
+	if (str.length() == 0) {
+	    return 0;
+	}
+	if (str.length() >= 3 && str.substring(0, 3).equals("abc") || str.substring(0, 3).equals("aba")) {
+	    return countAbc(str.substring(1)) + 1;
+	}
+	return countAbc(str.substring(1));
+    }
+}
